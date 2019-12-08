c8170218801eb3ace95e63588a79db22f31b1623
Zoican <zoican_denis@yahoo.com>
2019-12-08 15:00:11 +0200

    Added Week3-StringFunctions

---
+++ Week3-StringFunctions/Week3-StringFunctions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "Denis Zoican";
+
+            if (string.IsNullOrEmpty(s)) //First function
+            {
+                Console.WriteLine("String is empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Format("The string is:\n{0}", s)); //Seconds function
+            }
+
+            Console.WriteLine("First letter of my name is {0}", s[s.IndexOf("Denis")]); ///Third function
+
+            string s1 = " is a student";
+
+            Console.WriteLine(string.Concat(s,s1)); ///Fourth function
+
+            s = s.Replace("Denis", "Denis Alexandru"); ///Fifth function
+            Console.WriteLine(s);
+
+            if (s.Contains("Mihai")) ///sixth function
+            {
+                Console.WriteLine("Imposibil");
+            } else
+            {
+                Console.WriteLine("Pe mine nu ma cheama Mihai.");
+            }
+
+            Console.WriteLine(s.ToLower()); ///seventh function
+        }
+    }
+}
