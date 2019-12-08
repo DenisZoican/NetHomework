c0e54d5338a14f49f35c7e6e039ca23aa6ca6083
Zoican <zoican_denis@yahoo.com>
2019-12-08 14:37:32 +0200

    Added Week3-Person

---
+++ Week2-GuessNumber/Week2-GuessNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int nr = rnd.Next(1, 100);
+
+            while (true)
+            {
+                Console.WriteLine("Please input your number:");
+                string input = Console.ReadLine();
+                int numberInput = int.Parse(input);
+                if (numberInput == nr)
+                {
+                    Console.WriteLine("You won!");
+                    break;
+                }
+                else if (numberInput < nr)
+                {
+                    Console.WriteLine("Your number is smaller");
+                }
+                else
+                {
+                    Console.WriteLine("Your number is bigger");
+                }
+            }
+        }
+    }
+}
